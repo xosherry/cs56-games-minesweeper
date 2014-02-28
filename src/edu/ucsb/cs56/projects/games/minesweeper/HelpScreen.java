9b5a2d804c8ca9b1385d20d6e100144f11fff7f4
Julian Gee <jgee@umail.ucsb.edu>
2014-02-27 17:46:51 -0800

    HelpScreen.java has javadoc comments

--- src/edu/ucsb/cs56/projects/games/minesweeper/HelpScreen.java
+++ src/edu/ucsb/cs56/projects/games/minesweeper/HelpScreen.java
@@ -17,6 +17,12 @@ public class HelpScreen {
     JFrame temp;
     JPanel otherMenu;
     
+    /**
+     * This has a two arg constructor. It takes in the frame, and a panel representing the menu that you accessed help from. It creates a new panel, displaying the help message and a back button, which takes you back to the previous menu.
+@param frame - The Frame of the game
+@param panel - JPanel representing a menu
+    */
+
     public HelpScreen(JFrame frame, JPanel panel) {
 	this.temp = frame;
 	this.otherMenu = panel;
@@ -35,6 +41,10 @@ public class HelpScreen {
 	helpScreen.setVisible(true);
 	panel.setVisible(false);
    }
+
+    /** helper function to return to the previous menu
+     */
+
     public void back(){
 	temp.getContentPane().add(otherMenu);
 	helpScreen.setVisible(false);
