1b2ef810e67e3074f1216bb4cbe0bea3a4f4ab55
athielk <alexthielk@gmail.com>
2016-03-10 16:25:28 -0800

    Update MineComponent.java

--- src/edu/ucsb/cs56/projects/games/minesweeper/MineComponent.java
+++ src/edu/ucsb/cs56/projects/games/minesweeper/MineComponent.java
@@ -11,7 +11,7 @@ import javax.swing.JOptionPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentEvent;
 import java.awt.event.ComponentListener;
-import java.awt.event. ActionEvent;
+import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.Color;
 import java.util.ArrayList;
